f2aea789716b0642432882574bc24495ba4be091
pc-shop <khaledshehata492@gmail.com>
2024-12-28 12:08:31 +0200

    first commit

---
+++ lib/view/splash_screen/splash_view.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:fort_parts/main.dart';
+import 'package:get/get.dart';
+import 'package:is_first_run/is_first_run.dart';
+
+
+class SplashView extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+late bool first;
+bool isExpaded=false;
+firstTime()async {
+  bool firstRun = await IsFirstRun.isFirstRun();
+  first = firstRun;
+}
+
+
+class _MyHomePageState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    firstTime();
+    Timer(
+        const Duration(seconds: 8),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) =>  MyHomePage(title: 'hi'))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: Get.height,
+        width: Get.width,
+        child:
+            Container(
+                  width: Get.width*.4,
+                color: Colors.white,
+                child: Center(
+                    child: Stack(
+
+                  children: [
+                    AnimatedSize(
+
+                        curve: Curves.fastOutSlowIn,
+                      onEnd: (){
+                     setState(() {
+                       isExpaded=true;
+                     });
+                      },
+                      duration: Duration(seconds: 2),
+                        alignment: Alignment.center,
+                        child: Container(
+                          width: Get.width*2,
+                            height: Get.height*2,
+                            child: Image.asset('images/transitionwipe.png',fit: isExpaded?BoxFit.fitHeight:BoxFit.none,))),
+                    Container(
+                        width:  isExpaded?Get.width*.8:Get.width*.4,
+                        height: isExpaded?Get.height*.7:Get.height*.2,
+                        child: Image.asset('images/logo.png')),
+                  ],
+                      alignment: Alignment.center,
+                ))),
+
+
+
+      ),
+    );
+  }
+}
